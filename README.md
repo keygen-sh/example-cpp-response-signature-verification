42eb689a610ee490b508c7e7ed9b73cf36effbae
Zeke Gabrielse <ezekg@yahoo.com>
2022-06-17 08:11:53 -0500

    update example for ed25519 signatures

--- README.md
+++ README.md
@@ -1,59 +1,60 @@
 # Example C++ Response Signature Verification
 
-This is an example of cryptographically verifying Keygen's legacy `X-Signature` response signature header
-using your Keygen account's RSA public key. You can find your public key within
-[your account's settings page](https://app.keygen.sh/settings). We recommend
-using [our new `Keygen-Signature` header](https://keygen.sh/docs/api/#response-signatures),
-which better prevents replay attacks among other attack vectors. We will try
-to update this example repository for the new format soon.
+This is an example of cryptographically verifying [response signatures](https://keygen.sh/docs/api/signatures/#response-signatures)
+using your Keygen account's Ed25519 public key. Response signatures can help prevent replay
+attacks, among other attack vectors, such as a man-in-the-middle attack. You can find your
+public key within [your account's settings page](https://app.keygen.sh/settings).
+
+All dependencies are under `include/`, except for libcurl.
 
 ## Running the example
 
 First up, add an environment variable containing your public key:
+
 ```bash
-# Your Keygen account's public key (make sure it is *exact* - newlines and all)
-export KEYGEN_PUBLIC_KEY=$(printf %b \
-  '-----BEGIN PUBLIC KEY-----\n' \
-  'zdL8BgMFM7p7+FGEGuH1I0KBaMcB/RZZSUu4yTBMu0pJw2EWzr3CrOOiXQI3+6bA\n' \
-  # …
-  'efK41Ml6OwZB3tchqGmpuAsCEwEAaQ==\n' \
-  '-----END PUBLIC KEY-----')
+# Your Keygen account's Ed25519 public key
+export KEYGEN_PUBLIC_KEY="e8601e48b69383ba520245fd07971e983d06d22c4257cfd82304601479cee788"
+
+# Your Keygen account's ID
+export KEYGEN_ACCOUNT="1fddcec8-8dd3-4d8d-9b16-215cac0f9b52"
+
+# An API token
+export KEYGEN_TOKEN="activ-..."
 ```
 
 You can either run each line above within your terminal session before
 starting the app, or you can add the above contents to your `~/.bashrc`
 file and then run `source ~/.bashrc` after saving the file.
 
-On macOS, install OpenSSL v1.0.2p using `homebrew`.
+On macOS, compile the source using `g++`:
+
 ```bash
-brew install openssl@1.0.2
+g++ main.cpp -o bin.out -std=c++17 -stdlib=libc++ -lcurl -I include/**/*.c
 ```
 
-Next, on macOS, compile the source using `g++`:
+Then run the script, passing in an API endpoint path:
+
 ```bash
-g++ main.cpp -o bin.out \
-  -std=c++17 \
-  -lssl \
-  -lcrypto \
-  -I /usr/local/opt/openssl/include \
-  -L /usr/local/opt/openssl/lib
+./bin.out /me
 ```
 
-Then run the script, passing in the `body` and `signature` as arguments:
+Alternatively, you can prefix the below command with env variables, e.g.:
+
 ```bash
-./bin.out '{JSON_RESPONSE_BODY}' '{LEGACY_RESPONSE_SIGNATURE}'
+KEYGEN_PUBLIC_KEY=... KEYGEN_ACCOUNT=... KEYGEN_TOKEN=... ./bin.out /me
 ```
 
-The response body's signature will be verified using RSA-SHA256 with PKCS1 v1.5
-padding. Be sure to copy your public key and other arguments correctly - the response
-signature will fail validation if these are copied or included incorrectly. You can
-find your public key in [your account's settings](https://app.keygen.sh/settings).
+The response body's signature will be verified using Ed25519.
+
+You can find your public key in [your settings](https://app.keygen.sh/settings).
 
 ## Running on other platforms
 
 We are only including instructions on how to compile and run this example on
 macOS. If you'd like to create a PR with instructions for another platform,
 such as Windows or Linux, please feel free to open a PR.
+
+If you have any tips on how to improve the compilation, please open a PR.
 
 ## Questions?
 
--- main.cpp
+++ main.cpp
@@ -1,36 +1,94 @@
-#include "include/base64.h"
-#include <openssl/pem.h>
-#include <openssl/rsa.h>
-#include <openssl/sha.h>
-#include <openssl/bio.h>
+#include "include/ed25519/ed25519.h"
+#include "include/sha256/sha256.h"
+#include "include/base64/base64.h"
+#include <curl/curl.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <unordered_map>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
-using namespace std;
+// We don't need Ed25519 key generation
+#define ED25519_NO_SEED
 
-// Convert char array to a hex string
-constexpr char hexmap[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-
-string hex_str(unsigned char *data, int len)
+// trim removes whitespace and newline characters from a string.
+inline std::string trim(std::string str, std::string chars = " \t\n\r\f\v")
 {
-  string s(len * 2, ' ');
+  str.erase(str.find_last_not_of(chars) + 1);
+  str.erase(0, str.find_first_not_of(chars));
 
-  for (int i = 0; i < len; i++)
-  {
-    s[2 * i] = hexmap[(data[i] & 0xF0) >> 4];
-    s[2 * i + 1] = hexmap[data[i] & 0x0F];
-  }
-
-  return s;
+  return str;
 }
 
-// Add ANSII color codes to string
-string ansii_color_str(const string str, const int color_code)
+// lower converts a string to lowercase.
+inline std::string lower(std::string str)
 {
-  ostringstream stream;
+  std::transform(str.cbegin(), str.cend(), str.begin(), [](unsigned char c) { return std::tolower(c); });
+
+  return str;
+}
+
+// split splits a string by delimiter into a vector of strings.
+inline std::vector<std::string> split(std::string str, std::string delim, int n = 0)
+{
+  std::vector<std::string> vec;
+  size_t pos;
+
+  // Keep track of iterations for n
+  int i = 0;
+
+  while ((n == 0 || (n > 0 && i < n - 1)) && (pos = str.find(delim)) != std::string::npos)
+  {
+    vec.push_back(str.substr(0, pos));
+
+    str = str.substr(pos + delim.size());
+
+    i++;
+  }
+
+  vec.push_back(str); // Last word
+
+  return vec;
+}
+
+// sha256 calculates the SHA256 digest of a string, returning a base64 encoded string.
+inline std::string sha256(std::string str)
+{
+  uint8_t digest[32];
+  int l;
+
+  sha256_easy_hash(str.c_str(), str.size(), digest);
+
+  char* enc = base64(digest, 32, &l);
+
+  return std::string(enc);
+}
+
+// unhex convert a hex string to raw bytes.
+inline void unhex(std::string str, unsigned char* bytes)
+{
+  std::stringstream converter;
+
+  for (int i = 0; i < str.size(); i += 2)
+  {
+    int byte;
+
+    converter << std::hex << str.substr(i, 2);
+    converter >> byte;
+
+    bytes[i / 2] = byte & 0xff;
+
+    converter.str(std::string());
+    converter.clear();
+  }
+}
+
+// colorize adds ANSII color codes to a string.
+inline std::string colorize(const std::string str, const int color_code)
+{
+  std::stringstream stream;
 
   stream << "\033[1;";
   stream << color_code;
@@ -41,95 +99,278 @@ string ansii_color_str(const string str, const int color_code)
   return stream.str();
 }
 
-// Load an RSA public key from a PEM string
-RSA* load_rsa_pubic_key_from_pem(const string pem_pub_key)
+// signature represents a parsed response signature (https://keygen.sh/docs/api/signatures/#response-signatures).
+struct signature
 {
-  BIO* bio = BIO_new_mem_buf(pem_pub_key.c_str(), -1);
-  BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
+  std::string keyid;
+  std::string algorithm;
+  std::string signature;
+  std::string headers;
+};
 
-  RSA* rsa = PEM_read_bio_RSA_PUBKEY(bio, nullptr, nullptr, nullptr);
-  if (!rsa)
-  {
-    cerr << ansii_color_str("[ERROR]", 31) << " "
-         << "Failed to load public key"
-         << endl;
+// request represents an API request.
+struct request
+{
+  std::string host;
+  std::string method;
+  std::string path;
+};
 
-    exit(1);
-  }
+// response represents an API response.
+struct response
+{
+  request request;
+  int status;
+  std::unordered_map<std::string, std::string> headers;
+  std::string body;
+  signature signature;
+  std::string digest;
+  std::string date;
+};
 
-  // Should always be a 2048-bit public key
-  assert(2048/8 == RSA_size(rsa));
+// write_fn handles writing a curl response's body into a response.
+size_t write_fn(const char* ptr, size_t size, size_t nmemb, void* data)
+{
+  response& res = *reinterpret_cast<response*>(data);
 
-  BIO_free(bio);
+  res.body.insert(res.body.end(), ptr, ptr + size * nmemb);
 
-  return rsa;
+  return nmemb;
 }
 
-// Verify a request body's cryptographic signature
-bool verify_request_signature(RSA* rsa, const string body, const string sig)
+// header_fn handles writing a curl response's status and headers into a response.
+size_t header_fn(const char* ptr, size_t size, size_t nmemb, void* data)
 {
-  int body_len = body.size();
-  unsigned char* body_buf = reinterpret_cast<unsigned char *>(
-    const_cast<char *>(body.c_str())
-  );
+  response& res = *reinterpret_cast<response*>(data);
 
-  // Base64 decode signature
-  int sig_len;
-  unsigned char* sig_buf = unbase64(sig.c_str(), sig.size(), &sig_len);
+  auto bytes = size * nmemb;
+  auto end = ptr + bytes;
 
-  // Hash request body using SHA256
-  unsigned char body_digest[SHA256_DIGEST_LENGTH];
-  SHA256(body_buf, body_len, body_digest);
-
-  cout << ansii_color_str("[DIGEST]", 33) << " "
-       << hex_str(body_digest, SHA256_DIGEST_LENGTH)
-       << endl;
-
-  // Verify the request body's signature
-  int res = RSA_verify(
-    NID_sha256,
-    body_digest,
-    SHA256_DIGEST_LENGTH,
-    sig_buf,
-    sig_len,
-    rsa
-  );
-
-  RSA_free(rsa);
-
-  free(sig_buf);
-
-  return (bool) res;
-}
-
-int main(int argc, char* argv[])
-{
-  if (argc != 3)
+  if (bytes > 7 && std::string(ptr, ptr + 7) == "HTTP/1.")
   {
-    cerr << ansii_color_str("[ERROR]", 31) << " "
-         << "No request body or signature argument(s) specified"
-         << endl;
-
-    exit(1);
-  }
-
-  string pem_pub_key = getenv("KEYGEN_PUBLIC_KEY");
-  RSA* rsa = load_rsa_pubic_key_from_pem(pem_pub_key);
-
-  string body = argv[1];
-  string sig = argv[2];
-
-  bool res = verify_request_signature(rsa, body, sig);
-  if (res)
-  {
-    cout << ansii_color_str("[OK]", 32) << " "
-         << "Signature is valid!"
-         << endl;
+    if (bytes >= 12)
+    {
+      res.headers.clear();
+      res.status = std::stoi(std::string(ptr + 9, ptr + 12));
+    }
+    else
+    {
+      res.status = -1;
+    }
   }
   else
   {
-    cerr << ansii_color_str("[ERROR]", 31) << " "
-         << "Signature is not valid!"
-         << endl;
+    auto sep = std::find(ptr, end, ':');
+    if (sep != end)
+    {
+      std::string key = std::string(ptr, sep);
+      std::string value(sep + 1, end);
+
+      // Lowercase header keys
+      key = lower(key);
+
+      // Trim whitespace and newlines
+      value = trim(value);
+
+      res.headers[key] = std::move(value);
+    }
+  }
+
+  return nmemb;
+}
+
+// do_request sends an API request.
+response do_request(const std::string account_id, const std::string token, std::string path)
+{
+  static const std::string host = "api.keygen.sh";
+  static const std::string method = "GET";
+
+  // Ensure path has a leading slash
+  if (path.at(0) != '/')
+  {
+    path = '/' + path;
+  }
+
+  // Fully qualified path
+  path = "/v1/accounts/" + account_id + path;
+
+  // Perform the request
+  request req {host, method, path};
+  response res {req};
+
+  auto curl = curl_easy_init();
+  if (curl)
+  {
+    curl_easy_setopt(curl, CURLOPT_URL, ("https://" + host + path).c_str());
+
+    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_fn);
+    curl_easy_setopt(curl, CURLOPT_WRITEHEADER, reinterpret_cast<void*>(&res));
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_fn);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, reinterpret_cast<void*>(&res));
+
+    auto headers = curl_slist_append(nullptr, ("Authorization: Bearer " + token).c_str());
+    headers = curl_slist_append(headers, "Accept: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    auto res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+  }
+
+  // Parse signature header
+  if (res.headers.count("keygen-signature") > 0)
+  {
+    signature sig;
+
+    for (auto &param: split(res.headers["keygen-signature"], ", "))
+    {
+      auto kv = split(param, "=", 2);
+      auto k = trim(kv[0], " ");
+      auto v = trim(kv[1], "\"");
+
+      if (k == "keyid")
+      {
+        sig.keyid = v;
+      }
+
+      if (k == "algorithm")
+      {
+        sig.algorithm = v;
+      }
+
+      if (k == "signature")
+      {
+        sig.signature = v;
+      }
+
+      if (k == "headers")
+      {
+        sig.headers = v;
+      }
+    }
+
+    res.signature = sig;
+  }
+
+  // Convenience fields
+  res.digest = res.headers["digest"];
+  res.date = res.headers["date"];
+
+  return res;
+}
+
+// verify_response verifies a response's cryptographic signature using Ed25519.
+bool verify_response(const std::string pubkey, response res)
+{
+  if (res.signature.algorithm != "ed25519")
+  {
+    return false;
+  }
+
+  // Recreate signing data for signature
+  std::stringstream stream;
+
+  stream << "(request-target): " << lower(res.request.method) << " " << lower(res.request.path) << "\n"
+         << "host: " << res.request.host << "\n"
+         << "date: " << res.date << "\n"
+         << "digest: sha-256=" << sha256(res.body);
+
+  // Convert signing data into bytes
+  auto data = stream.str();
+  auto data_bytes = reinterpret_cast<const unsigned char*>(data.c_str());
+  auto data_size = data.size();
+
+  // Decode signature into bytes
+  auto sig = res.signature.signature;
+  int sig_size;
+
+  unsigned char* sig_bytes = unbase64(sig.c_str(), sig.size(), &sig_size);
+
+  // Decode hex public key into bytes
+  unsigned char key_bytes[32];
+
+  unhex(pubkey, key_bytes);
+
+  // Verify signature
+  auto ok = ed25519_verify(sig_bytes, data_bytes, data_size, key_bytes);
+
+  return (bool) ok;
+}
+
+// main runs the example program.
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << colorize("[ERROR]", 31) << " "
+              << "No path given"
+              << std::endl;
+
+    return 1;
+  }
+
+  if (!getenv("KEYGEN_PUBLIC_KEY"))
+  {
+    std::cerr << colorize("[ERROR]", 31) << " "
+              << "Environment variable KEYGEN_PUBLIC_KEY is missing"
+              << std::endl;
+
+    return 1;
+  }
+
+  if (!getenv("KEYGEN_ACCOUNT"))
+  {
+    std::cerr << colorize("[ERROR]", 31) << " "
+              << "Environment variable KEYGEN_ACCOUNT is missing"
+              << std::endl;
+
+    return 1;
+  }
+
+  if (!getenv("KEYGEN_TOKEN"))
+  {
+    std::cerr << colorize("[ERROR]", 31) << " "
+              << "Environment variable KEYGEN_TOKEN is missing"
+              << std::endl;
+
+    return 1;
+  }
+
+  std::string pubkey = getenv("KEYGEN_PUBLIC_KEY");
+  std::string account = getenv("KEYGEN_ACCOUNT");
+  std::string token = getenv("KEYGEN_TOKEN");
+  std::string path = argv[1];
+
+  // Perform the request
+  auto res = do_request(account, token, path);
+
+  std::cout << "host=" << colorize(res.request.host, 34) << "\n"
+            << "method=" << colorize(res.request.method, 34) << "\n"
+            << "path=" << colorize(res.request.path, 34) << "\n"
+            << "status=" << colorize(std::to_string(res.status), 34) << "\n"
+            << "date=" << colorize(res.date, 34) << "\n"
+            << "signature=" << colorize(res.signature.signature, 34) << "\n"
+            << "algorithm=" << colorize(res.signature.algorithm, 34) << "\n"
+            << "digest=" << colorize(res.digest, 34) << "\n"
+            << "body=" << colorize(res.body, 34)
+            << std::endl;
+
+  // Verify the response signature
+  bool ok = verify_response(pubkey, res);
+  if (ok)
+  {
+    std::cout << colorize("[OK]", 32) << " "
+              << "Signature is valid!"
+              << std::endl;
+
+    return 0;
+  }
+  else
+  {
+    std::cerr << colorize("[ERROR]", 31) << " "
+              << "Signature is not valid!"
+              << std::endl;
+
+    return 1;
   }
 }
